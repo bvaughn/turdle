b502d65ca182238f6f38b742023368a67644c87d
Brian Vaughn <bvaughn@fb.com>
2022-02-17 19:05:06 -0500

    Fixed copy-to-clipboard for iOS/Safari

--- utils/copy.js
+++ utils/copy.js
@@ -6,23 +6,54 @@ import {
 } from "../constants";
 
 export function copyEndGameStatus(state) {
-  navigator.clipboard.writeText(
+  const textToCopy =
     `https://turdle.app (${state.submittedGuesses.length}/${MAX_GUESSES})\n\n` +
-      state.submittedGuesses
-        .map((guess) => {
-          return guess
-            .map(([_, status]) => {
-              switch (status) {
-                case STATUS_CORRECT:
-                  return "🟩";
-                case STATUS_INCORRECT:
-                  return "⬛";
-                case STATUS_PRESENT:
-                  return "🟨";
-              }
-            })
-            .join("");
-        })
-        .join("\n")
-  );
+    state.submittedGuesses
+      .map((guess) => {
+        return guess
+          .map(([_, status]) => {
+            switch (status) {
+              case STATUS_CORRECT:
+                return "🟩";
+              case STATUS_INCORRECT:
+                return "⬛";
+              case STATUS_PRESENT:
+                return "🟨";
+            }
+          })
+          .join("");
+      })
+      .join("\n");
+
+  try {
+    copyTextNavigatorClipboard(textToCopy);
+  } catch (error) {
+    // navigator.clipboard.writeText() doesn't work on iOS,
+    // so fall back to using the browser "copy" command.
+    copyTextDOMElement(textToCopy);
+  }
+}
+
+function copyTextNavigatorClipboard(textToCopy) {
+  navigator.clipboard.writeText(textToCopy);
+}
+
+function copyTextDOMElement(textToCopy) {
+  let textArea = document.createElement("textArea");
+  textArea.value = textToCopy;
+
+  document.body.appendChild(textArea);
+
+  const range = document.createRange();
+  range.selectNodeContents(textArea);
+
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  textArea.setSelectionRange(0, 999999);
+
+  document.execCommand("copy");
+
+  document.body.removeChild(textArea);
 }
